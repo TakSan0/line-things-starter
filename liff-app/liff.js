a4a93bdf5d9b9cab3741f44dae3e07ac9bae373e
TakSan0 <34002032+TakSan0@users.noreply.github.com>
2019-05-19 08:37:06 +0900

    Change SSID

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'cb3ddf46-85bc-4e37-aa42-1c2073893558'; // LED, Button
+const USER_SERVICE_UUID         = 'f2ced314-4862-418b-871c-29d359403f03'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
